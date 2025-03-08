a9072fab175324baa9cc7ff89ea977a0a06350d0
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2025-03-08 19:53:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from io import BytesIO
 
 st.set_page_config(layout="wide", page_title="Mistral OCR App", page_icon="🖥️")
 st.title("Get accurate and perfect results from your pdf")
-st.markdown("<h3 style color: white;'>Built by <a href='https://github.com/AIAnytime'>AI Anytime with ❤️ </a></h3>", unsafe_allow_html=True)
+#st.markdown("<h3 style color: white;'>Built by <a href='https://github.com/AIAnytime'>AI Anytime with ❤️ </a></h3>", unsafe_allow_html=True)
 
 # 1. API Key Input
 api_key ="h98KHKhrGdjHWi2pOwz46F7bWUMwWWk7"
