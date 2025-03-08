fee69351d1fcf2ed592b4584db64b5278032d5cd
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2025-03-08 20:43:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.title("Advanced Pdf Processor App")
 #st.markdown("<h3 style color: white;'>Built by <a href='https://github.com/AIAnytime'>AI Anytime with ❤️ </a></h3>", unsafe_allow_html=True)
 with st.expander("Expand Me"):
     st.markdown("""
-    This application allows you to extract information from pdf/image based on Mistral OCR. Built by AI Anytime.
+    This application allows you to extract information from pdf/image based on advanced OCR Processor
     """)
 
 # 1. API Key Input
